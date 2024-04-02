2f547cfb29b9300a1c422fa776739a8829938dfe
subrata6630 <subrata6630@gmail.com>
2024-04-02 20:22:30 +0600

    template syntex

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,38 @@
 <script setup>
 
+let msg = "Hello world!";
+
+let dynamicId ="2"
+
+let isButonDisabled = true
+
 </script>
+
+
+
+
 
 <template>
 
-  App
+<!-- Print v-html -->
+<!--Print {{ msg }} -->
+
+
+  <div v-html="msg"></div>
+
+
+<!-- Dynamic id bind. Please Check inscpect  element to see the result-->
+
+  <div v-bind:id ="dynamicId">Dynamic id :  {{ dynamicId}} </div>
+
+<!-- Another Style  of binding data with attribute (Short hand bind) -->
+
+<div :id ="dynamicId">Dynamic id :  {{ dynamicId}} </div>
+
+
+<button :disabled="isButonDisabled">Click me!</button>
+
+
   
 </template>
 
